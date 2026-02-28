30e79da47591d72597b89df779d25364dfb86d71
qlfzn <aqilfauzan09@gmail.com>
2026-02-28 09:29:01 +0800

    add Dockerfile

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[ALLOW_ORIGINS],
     allow_methods=["GET", "POST"],
-    allow_headers=["application/pdf"],
+    allow_headers=["*"],
 )
 
 app.include_router(router, prefix="/api/v1")
